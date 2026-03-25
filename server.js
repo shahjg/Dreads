048dbfa6aea7618aeb874c6983be8a57282a5736
shahjg <165319445+shahjg@users.noreply.github.com>
2026-03-25 15:55:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 app.use(session({ secret: 'dreads-key', resave: false, saveUninitialized: true }));
 
 app.get('/proxy', async (req, res) => {
