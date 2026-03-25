8996e4d692137c8d6b281bb69f6f133533e4ad21
shahjg <165319445+shahjg@users.noreply.github.com>
2026-03-25 15:56:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(__dirname + '/index.html');
 });
 app.use(session({ secret: 'dreads-key', resave: false, saveUninitialized: true }));
 
